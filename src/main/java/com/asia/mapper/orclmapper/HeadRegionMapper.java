17d342c9f9ea29f81f7d3dbfc5d170f5dbfb974f
kafkawangxiansheng <416692807@qq.com>
2019-10-20 16:02:18 +0800

    2019.10.20接口调整

--- src/main/java/com/asia/mapper/orclmapper/HeadRegionMapper.java
+++ src/main/java/com/asia/mapper/orclmapper/HeadRegionMapper.java
@@ -1,5 +1,7 @@
 package com.asia.mapper.orclmapper;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +12,7 @@ import java.util.Map;
  * @date 2019/10/1521:47
  * @Version 1.0
  */
+@Repository
 public interface HeadRegionMapper {
     List<Map<String, Object>> selectHeadRegion(Map map);
 }
