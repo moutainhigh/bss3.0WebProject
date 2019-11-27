a29e60cb97c46f8de794843246026668464105d6
kafkawangxiansheng <416692807@qq.com>
2019-11-27 15:47:40 +0800

    充值接口增加支持账户id充值_20191127

--- src/main/java/com/asia/mapper/billingmapper/AReturnRuleInstanceMapper.java
+++ src/main/java/com/asia/mapper/billingmapper/AReturnRuleInstanceMapper.java
@@ -3,13 +3,12 @@ package com.asia.mapper.billingmapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public interface AReturnRuleInstanceMapper {
     String queryAReturnRuleInstance(@Param("acct_id") String acct_id,
                                           @Param("return_rule_id") String return_rule_id);
-
+    List<Map<String,Object>> queryAReturnRuleStep(@Param("return_rule_id") String return_rule_id);
 }
